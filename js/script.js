96a3c98b230ba353abb38f4c29e10863d5a41c68
Carlos García <carlosgrcia@yahoo.com>
2025-01-03 02:36:20 -0600

    Refactor index.html and script.js to improve pet selection functionality. Added event listener for pet selection and updated radio button ID for 'Capipeo' to 'Capipepo'. Included script tag in HTML for better organization.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-function seleccionarMascota() {
-    alert("Mascota seleccionada");
+function cargarEventos() {
+    let botonMascota = document.getElementById("boton-mascota");
+    botonMascota.addEventListener("click", seleccionarMascota);
 }
 
-let botonMascota = document.getElementById("boton-mascota");
-botonMascota.addEventListener("click", seleccionarMascota);
+function seleccionarMascota() {
+    let inputHipodoge = document.getElementById("hipodoge");
+    let inputCapipepo = document.getElementById("capipepo");
+    let inputRatigueya = document.getElementById("ratigueya");
+    let inputLangostelvis = document.getElementById("langostelvis");
+    let inputTucapalma = document.getElementById("tucapalma");
+    let inputPidgit = document.getElementById("pidgit");
+
+    if (inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge");
+    } else if (inputCapipepo.checked) {
+        alert("Seleccionaste a Capipepo");
+    } else if (inputRatigueya.checked) {
+        alert("Seleccionaste a Ratigueya");
+    } else if (inputLangostelvis.checked) {
+        alert("Seleccionaste a Langostelvis");
+    } else if (inputTucapalma.checked) {
+        alert("Seleccionaste a Tucapalma");
+    } else if (inputPidgit.checked) {
+        alert("Seleccionaste a Pidgit");
+    } else {
+        alert("No seleccionaste ninguna mascota");
+    }
+}
+
+window.addEventListener("load", cargarEventos);
