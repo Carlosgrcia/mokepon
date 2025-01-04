d292b88da0ec582d72f41b003b704cc66afdac6d
Carlos García <carlosgrcia@yahoo.com>
2025-01-04 15:42:03 -0600

    Add enemy pet selection functionality in script.js. Introduced a new function to randomly select an enemy pet based on a random attack value, enhancing gameplay dynamics. Updated existing functions for better organization and clarity.

--- js/script.js
+++ js/script.js
@@ -33,6 +33,31 @@ function seleccionarMascota() {
     } else {
         alert("No seleccionaste ninguna mascota");
     }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,6)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio === 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodoge";
+    } else if (ataqueAleatorio === 2) {
+        spanMascotaEnemigo.innerHTML = "Capipepo";
+    } else if (ataqueAleatorio === 3) {
+        spanMascotaEnemigo.innerHTML = "Ratigueya";
+    } else if (ataqueAleatorio === 4) {
+        spanMascotaEnemigo.innerHTML = "Langostelvis";
+    } else if (ataqueAleatorio === 5) {
+        spanMascotaEnemigo.innerHTML = "Tucapalma";
+    } else if (ataqueAleatorio === 6) {
+        spanMascotaEnemigo.innerHTML = "Pidgit";
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 window.addEventListener("load", cargarEventos);
